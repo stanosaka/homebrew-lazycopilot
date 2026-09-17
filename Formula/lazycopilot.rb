b3216499df952e452d3c99058cc32a318835845b
Stan Zhou <stan.osaka@gmail.com>
2026-09-17 17:38:48 +1000

    Add lazycopilot formula

---
+++ Formula/lazycopilot.rb
@@ -0,0 +1,18 @@
+class Lazycopilot < Formula
+  desc "AI-powered commit message generator using GitHub Copilot"
+  homepage "https://github.com/stanosaka/lazycopilot"
+  url "https://github.com/stanosaka/lazycopilot/archive/refs/tags/v1.2.1-stz1.tar.gz"
+  sha256 "66ffe827087a4bce6a33fd7cdab00c943fad922d03acdf67bdbdbc2bf3edf903"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    system "make", "build"
+    bin.install "bin/lazycopilot"
+  end
+
+  test do
+    assert_match "lazycopilot", shell_output("#{bin}/lazycopilot --help", 0)
+  end
+end
